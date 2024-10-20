07046a9bab56436e613f628b60aff8830c66c294
Ghanshyam <tghanshyam512@gmail.com>
2024-10-20 13:59:36 +0530

    Update mean-continuous.cpp

--- mean-continuous.cpp
+++ mean-continuous.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <iomanip> // For setting decimal precision
 using namespace std;
 
 int main() {
     int n, classInterval;
-    float N = 0.0, sumFM = 0, mean, l;
+    float N = 0.0, sumFM = 0, mean, l, u;
 
     cout<<"Enter the n : ";
     cin>>n;
@@ -13,29 +12,21 @@ int main() {
     cout<<"Enter the class interval : ";
     cin>>classInterval;
 
-    float lowerLimit[n], upperLimit[n];
-    float frequencies[n];
-    float midVlaue[n]; 
-    float fm[n];
+    float f[n], m[n], fm[n];
 
     for(int i = 0; i<n; i++){
-        lowerLimit[i] = l;
-        upperLimit[i] = l+classInterval;
-        l = l+classInterval;
-        cout << "Enter the frequency for class " << i + 1 << " (" << lowerLimit[i] << " - " << upperLimit[i] << "): ";
-        cin >> frequencies[i];
-        midVlaue[i] = (lowerLimit[i]+upperLimit[i])/2;
-        fm[i] = midVlaue[i]* frequencies[i];
-        N += frequencies[i];
+        u = l+classInterval;
+        cout << "Enter the frequency for class " << i + 1 << " (" << l << " - " << u << "): ";
+        cin >> f[i];
+        m[i] = (l+u)/2;
+        fm[i] = m[i]* f[i];
+        N += f[i];  
         sumFM += fm[i];
+        
+        l = l+classInterval;
     }
 
-    cout<<endl<<"Data : "<<endl<<endl;
-    for(int i = 0; i<n; i++){
-        cout<<"C.I. : "<<lowerLimit[i]<<" - "<<upperLimit[i]<< " and f : " <<frequencies[i] <<" and m : "<<midVlaue[i]<<" amd fm : "<<fm[i]<<endl;
-    }
-
-    cout<<"N : "<<N<<endl;
+    cout<<endl<<"N : "<<N<<endl;
     cout<<"sum of fm : "<<sumFM<<endl;
     mean = sumFM/N;
     cout<<"Mean of the entered continuous series is : " << mean;
