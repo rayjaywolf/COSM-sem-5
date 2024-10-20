920655646253a3a0c86afb9c97bb8de2e8991077
Ghanshyam <tghanshyam512@gmail.com>
2024-10-20 17:43:54 +0530

    Update gm-discrete.cpp

--- gm-discrete.cpp
+++ gm-discrete.cpp
@@ -5,25 +5,18 @@ using namespace std;
 
 int main() {
     int n;
-    float first, interval;
     double sumFLogX = 0.0, geometricMean, N = 0;
 
     // Input number of elements
     cout << "Enter the number of elements (n): ";
     cin >> n;
-
-    // Input first variable and interval
-    cout << "Enter the first variable: ";
-    cin >> first;
-    cout << "Enter the interval: ";
-    cin >> interval;
-
     float frequencies[n]; // Array to hold frequencies
     float x; // To hold the variable value
 
     // Loop to input frequencies and calculate sum of f*log(x)
     for (int i = 0; i < n; i++) {
-        x = first + i * interval; // Calculate current variable
+        cout << "Enter the " << i + 1 << "th variable of series: ";
+        cin>>x; // Calculate current variable
         cout << "Enter the frequency for variable " << x << ": ";
         cin >> frequencies[i];
 
@@ -35,8 +28,6 @@ int main() {
     // Calculate the geometric mean using the formula:
     geometricMean = exp(sumFLogX / N);
 
-    // Display the result
-    cout << fixed << setprecision(5); // Set precision for output
     cout << "Geometric Mean of the entered discrete frequency distribution is: " << geometricMean << endl;
 
     return 0;
