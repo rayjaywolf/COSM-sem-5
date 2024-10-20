3a9d94f00eb3a1cf4a4b398a789c62c48af66464
Ghanshyam <tghanshyam512@gmail.com>
2024-10-20 14:00:45 +0530

    Update mean-individual.cpp

--- mean-individual.cpp
+++ mean-individual.cpp
@@ -1,36 +1,3 @@
-// #include <iostream>
-// using namespace std;
-
-// int main() {
-//     int n;
-//     float sum = 0.0, mean;
-    
-//     cout << "Enter the number of elements in the series: ";
-//     cin >> n;
-
-//     // Declare the array with size n
-//     float series[n];
-
-//     // Input the series elements
-//     for (int i = 0; i < n; i++) {
-//         cout << "Enter the " << i+1 << "th number of the series: ";
-//         cin >> series[i];
-//     }
-
-//     // Output the series elements and calculate the sum
-//     cout << "The series elements are:" << endl;
-//     for (int i = 0; i < n; i++) {
-//         cout << series[i] << endl;
-//         sum += series[i]; // Corrected from series[1] to series[i]
-//     }
-
-//     cout << "Sum of the series: " << sum << endl;
-
-//     mean = sum/n;
-//     cout<<"Mean of the individual series is : "<<mean;
-//     return 0;
-// }
-
 #include <iostream>
 using namespace std;
 
