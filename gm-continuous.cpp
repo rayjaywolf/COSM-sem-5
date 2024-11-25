f342ef4e2c195333744aa2e338a20c154a3e7ae6
Rehan Jinta <jintarehaan@gmail.com>
2024-11-25 11:36:11 +0530

    Update gm-continuous.cpp

--- gm-continuous.cpp
+++ gm-continuous.cpp
@@ -9,13 +9,13 @@ int main() {
     double sumFLogM = 0.0, geometricMean;
 
     // Input number of classes
-    cout << "Enter the number of classes (n): ";
+    cout << "Number of classes (n): ";
     cin >> n;
 
     // Input lower limit of first class and class interval
-    cout << "Enter the lower limit of the first class: ";
+    cout << "Lower Limit: ";
     cin >> lowerLimit;
-    cout << "Enter the class interval: ";
+    cout << "Class interval: ";
     cin >> interval;
 
     float frequencies[n]; // Array to hold frequencies
@@ -26,7 +26,7 @@ int main() {
         float upperLimit = lowerLimit + interval; // Calculate the upper limit of the class
         midValue = (lowerLimit + upperLimit) / 2; // Calculate the midpoint
 
-        cout << "Enter the frequency for class " << i + 1 << " (" << lowerLimit << " - " << upperLimit << "): ";
+        cout << "Frequency for class " << i + 1 << " (" << lowerLimit << " - " << upperLimit << "): ";
         cin >> frequencies[i];
 
         // Calculate sum of f*log(midpoint)
@@ -42,7 +42,7 @@ int main() {
 
     // Display the result
     cout << fixed << setprecision(5); // Set precision for output
-    cout << "Geometric Mean of the entered continuous frequency distribution is: " << geometricMean << endl;
+    cout << "GM is: " << geometricMean << endl;
 
     return 0;
 }
